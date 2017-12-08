53fe1998d23492e808bab1ebfeed4af30786a9ce
dev richie <33795818+richiever@users.noreply.github.com>
2017-12-09 10:27:36 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,19 @@ client.on('message', message => {
     if (message.content === `${prefix}ping`) {
     	message.channel.send('PONG!');
       }
+    if (message.content == `${prefix}purge`) {
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+        message.channel.fetchMessages()
+           .then(function(list){
+                message.channel.bulkDelete(list);
+            }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})
+    }
+}
+
+
+return;
+ }
+});
 });
 
 // THIS  MUST  BE  THIS  WAY
